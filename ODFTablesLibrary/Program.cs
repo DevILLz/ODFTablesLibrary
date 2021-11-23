bafdc0cb103cd0253480029b8fd8e4cdeb907283
Mark <serov@angstrem.tech>
2021-11-23 16:33:10 +0300

    Добавьте файлы проекта.

---
+++ ODFTablesLibrary/Program.cs
@@ -0,0 +1,13 @@
+﻿using ODFTablesLib;
+using System.IO;
+
+public static class App
+{
+
+    static void Main()
+    {
+        new ODFTables(Path.GetFullPath(@"NewTemplate.odt")).Save(@"C:\1.odt");
+
+
+    }
+}
